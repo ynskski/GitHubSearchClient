de67e82046eddee209f5cc5e42667fa4522d5172
Yunosuke <qmkns246@gmail.com>
2020-05-06 19:06:15 +0900

    Initial Commit

---
+++ GitHubSearchClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubSearchClient
+//
+//  Created by 堺雄之介 on 2020/05/06.
+//  Copyright © 2020 堺雄之介. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
